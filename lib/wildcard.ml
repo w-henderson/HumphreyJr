6185061c8f9df19be1f77f9d0224a820812ecfca
William Henderson <william-henderson@outlook.com>
2022-09-10 10:57:20 +0100

    Optimised wildcard algorithm for tailing wildcards

--- lib/wildcard.ml
+++ lib/wildcard.ml
@@ -2,7 +2,7 @@ let wildcard_match p t =
   let nth s n = if n < String.length s then Some (String.get s n) else None in
   let rec m i j =
     match (nth p i, nth t j) with
-    | Some '*', Some _ -> m (i + 1) j || m i (j + 1)
+    | Some '*', Some _ -> m i (j + 1) || m (i + 1) j
     | Some '*', None -> m (i + 1) j
     | Some s_i, Some s_j -> s_i = s_j && m (i + 1) (j + 1)
     | Some _, None -> false
